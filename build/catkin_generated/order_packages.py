6bf4d85da642697a3820cfbd91c4235c4d6edb72
RainWithApril <107609367+RainWithApril@users.noreply.github.com>
2022-07-04 11:47:54 -0400

    Add files via upload

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/autorobot/experiments/220625/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/autorobot/experiments/220625/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/autorobot/experiments/220625/catkin_ws/devel;/opt/ros/melodic' != '' else []

3ecf3bdc54e294ae33e1fa3a739518a046e77997
RainWithApril <107609367+RainWithApril@users.noreply.github.com>
2022-07-04 11:04:44 -0400

    Add files via upload

---
+++ devel/share/gennodejs/ros/learning_communication/msg/_index.js
@@ -0,0 +1,22 @@
+
+"use strict";
+
+let Person = require('./Person.js');
+let DoDishesActionResult = require('./DoDishesActionResult.js');
+let DoDishesResult = require('./DoDishesResult.js');
+let DoDishesActionFeedback = require('./DoDishesActionFeedback.js');
+let DoDishesActionGoal = require('./DoDishesActionGoal.js');
+let DoDishesGoal = require('./DoDishesGoal.js');
+let DoDishesAction = require('./DoDishesAction.js');
+let DoDishesFeedback = require('./DoDishesFeedback.js');
+
+module.exports = {
+  Person: Person,
+  DoDishesActionResult: DoDishesActionResult,
+  DoDishesResult: DoDishesResult,
+  DoDishesActionFeedback: DoDishesActionFeedback,
+  DoDishesActionGoal: DoDishesActionGoal,
+  DoDishesGoal: DoDishesGoal,
+  DoDishesAction: DoDishesAction,
+  DoDishesFeedback: DoDishesFeedback,
+};

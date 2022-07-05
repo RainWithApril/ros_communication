3e7f9d94908b35916e703ee3aa6d712eba28ed00
RainWithApril <107609367+RainWithApril@users.noreply.github.com>
2022-07-04 21:09:02 -0400

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/autorobot/experiments/220625/catkin_ws/src
+# Build directory: /home/autorobot/experiments/220625/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("learning_communication")
+subdirs("learning_tf")
